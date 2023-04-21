130cd2d0416247f15755b19bba5fa2e84f4bc8f5
sato-s <s.sato.desu@gmail.com>
2023-04-22 05:43:58 +0900

    minimum websocket server

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/websocket"
+)
+
+func main() {
+	app := iris.New()
+	ws := websocket.New(websocket.DefaultGorillaUpgrader, websocket.Events{
+		websocket.OnNativeMessage: func(nsConn *websocket.NSConn, msg websocket.Message) error {
+			log.Printf("Server got: %s from [%s]", msg.Body, nsConn.Conn.ID())
+
+			nsConn.Conn.Server().Broadcast(nsConn, msg)
+			return nil
+		},
+	})
+
+	// ws.OnConnect = func(c *websocket.Conn) error {
+	// 	log.Printf("[%s] Connected to server!", c.ID())
+	// 	return nil
+	// }
+	//
+	// ws.OnDisconnect = func(c *websocket.Conn) {
+	// 	log.Printf("[%s] Disconnected from server", c.ID())
+	// }
+
+	app.HandleDir("/js", iris.Dir("./static/js")) // serve our custom javascript code.
+
+	app.Get("/my_endpoint", websocket.Handler(ws))
+
+	app.Listen(":8080")
+}
