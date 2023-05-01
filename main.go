fcbab40b695933a2d9f5e2be8ec2d4314f598a9d
sato-s <s.sato.desu@gmail.com>
2023-05-01 17:07:56 +0900

    port variable

--- main.go
+++ main.go
@@ -1,41 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/websocket"
-	"github.com/kataras/neffos"
-)
-
-func onChat(ns *neffos.NSConn, msg neffos.Message) error {
-	// ctx := websocket.GetContext(ns.Conn)
-	// log.Println(string(msg.Body))
-	// fmt.Printf("%+v\n", msg)
-	// fmt.Printf("%+v\n", ctx)
-	return nil
-}
-
-func OnRoomJoined(ns *neffos.NSConn, msg neffos.Message) error {
-	ctx := websocket.GetContext(ns.Conn)
-	log.Println("====RoomJoined====")
-	fmt.Printf("%+v\n", msg)
-	fmt.Printf("%+v\n", ctx)
-	return nil
-}
-
 func main() {
-	app := iris.New()
-	ws := neffos.New(websocket.DefaultGorillaUpgrader, neffos.Namespaces{
-		"default": neffos.Events{
-			"chat":                    onChat,
-			neffos.OnNamespaceConnect: OnRoomJoined,
-			neffos.OnRoomJoin:         OnRoomJoined,
-			"OnRoomJoined":            OnRoomJoined,
-		},
-	})
-	app.Get("/", websocket.Handler(ws))
-
-	app.Listen(":8080")
+	NewServer(8080)
 }
---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/websocket"
+	"github.com/kataras/neffos"
+)
+
+type Server struct {
+	port uint
+}
+
+func NewServer(port uint) *Server {
+	s := &Server{port: port}
+	s.run()
+	return s
+}
+
+func (s *Server) run() {
+	app := iris.New()
+	ws := neffos.New(websocket.DefaultGorillaUpgrader, neffos.Namespaces{
+		"default": neffos.Events{
+			"chat":                    s.onChat,
+			neffos.OnNamespaceConnect: s.OnRoomJoined,
+			neffos.OnRoomJoin:         s.OnRoomJoined,
+			"OnRoomJoined":            s.OnRoomJoined,
+		},
+	})
+	app.Get("/", websocket.Handler(ws))
+
+	app.Listen(fmt.Sprintf(":%d", s.port))
+}
+
+func (s *Server) onChat(ns *neffos.NSConn, msg neffos.Message) error {
+	// ctx := websocket.GetContext(ns.Conn)
+	// log.Println(string(msg.Body))
+	// fmt.Printf("%+v\n", msg)
+	// fmt.Printf("%+v\n", ctx)
+	return nil
+}
+
+func (s *Server) OnRoomJoined(ns *neffos.NSConn, msg neffos.Message) error {
+	ctx := websocket.GetContext(ns.Conn)
+	log.Println("====RoomJoined====")
+	fmt.Printf("%+v\n", msg)
+	fmt.Printf("%+v\n", ctx)
+	return nil
+}
