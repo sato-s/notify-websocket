ddcb8aab470f12cb15c9ba68f4e2d9a7f8253f2c
sato-s <s.sato.desu@gmail.com>
2023-09-02 02:16:44 +0900

    wip

--- frontend/app/lib/Room.ts
+++ frontend/app/lib/Room.ts
@@ -1,11 +1,17 @@
+import * as neffos from "neffos.js";
 
 export default class Room {
   url: string;
 
   constructor(url: string) {
     this.url = url;
+    this.conn = await neffos.dial(url, {
+    });
   }
 
-  connect() {
+  _OnNamespaceConnected(nsConn, msg) {
   }
+  _OnNamespaceDisconnect(nsConn, msg) {
+  }
+
 }
