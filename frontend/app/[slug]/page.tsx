6feb92eb73048e6d06c279c6132cf0d5cff59aaa
sato-s <s.sato.desu@gmail.com>
2023-09-02 00:21:05 +0900

    wip

--- frontend/app/[slug]/page.tsx
+++ frontend/app/[slug]/page.tsx
@@ -2,6 +2,6 @@
 
 import React from "react";
 
-export default function Room() {
-  return <>room</>
+export default function Room({params}: {params: {slug: string}}) {
+  return <div>My Post: {params.slug}</div>
 }
